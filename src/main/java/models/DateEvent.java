68b6aca006335a2f9d5b908a9f8d0a48a973f723
Trosalio <thanapong.su@ku.th>
2017-08-30 23:02:58 +0700

    Initial Message

---
+++ src/main/java/models/DateEvent.java
@@ -0,0 +1,37 @@
+package models;
+
+import java.time.LocalDate;
+
+public class DateEvent {
+
+    private String eventName;
+    private LocalDate eventDate;
+    private String eventDescription;
+
+    public DateEvent() {
+    }
+
+    public void setEventName(String eventName) {
+        this.eventName = eventName;
+    }
+
+    public void setEventDate(LocalDate eventDate) {
+        this.eventDate = eventDate;
+    }
+
+    public void setEventDescription(String eventDescription) {
+        this.eventDescription = eventDescription;
+    }
+
+
+    @Override
+    public String toString() {
+        return "DateEvent{" +
+                "eventName='" + eventName + '\'' +
+                ", eventDate=" + eventDate +
+                ", eventDescription='" + eventDescription + '\'' +
+                '}';
+    }
+
+
+}
---
+++ src/main/java/views/AlertBox.java
@@ -0,0 +1,37 @@
+package views;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+
+public class AlertBox {
+
+    public static void display(String title, String message) throws IOException {
+        Stage window = new Stage();
+        window.setResizable(false);
+        window.initModality(Modality.APPLICATION_MODAL);
+        window.setTitle(title);
+        window.setMinHeight(100);
+
+        Label label = new Label();
+        label.setText(message);
+        Button closeButton = new Button("close");
+        closeButton.setOnAction(e -> window.close());
+
+        VBox layout = new VBox(10);
+        layout.getChildren().addAll(label,closeButton);
+        layout.setAlignment(Pos.CENTER);
+        Scene scene = new Scene(layout);
+        window.setScene(scene);
+        window.showAndWait();
+
+
+    }
+}
