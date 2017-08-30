4ef5b54fa30aea2d30891b729bf85351b3473e35
Trosalio <thanapong.su@ku.th>
2017-08-30 23:00:09 +0700

    Initial Message

--- src/main/java/CalendarApp.java
+++ src/main/java/CalendarApp.java
@@ -13,10 +13,9 @@ public class CalendarApp extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("calendarUI.fxml"));
-        loader.setControllerFactory(t -> new CalendarController());
-
-        stage.setScene(new Scene(loader.load()));
+        FXMLLoader calendarUILoader = new FXMLLoader(getClass().getResource("calendarUI.fxml"));
+        Scene calendarUI = new Scene(calendarUILoader.load());
+        stage.setScene(calendarUI);
         stage.setTitle("Event Window");
         stage.setResizable(false);
         stage.show();
