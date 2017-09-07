6ead5ba61df074f8f8508ae70565fd79e1b48487
Trosalio <thanapong.su@ku.th>
2017-09-07 18:26:04 +0700

    Add Test Classes

---
+++ src/test/java/models/DateEventTest.java
@@ -0,0 +1,52 @@
+package models;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created By:
+ * -Name:   Thanapong Supalak
+ * -ID:     5810405029
+ * Project Name: Calendar
+ */
+
+class DateEventTest {
+
+    private static DateEvent mockedEvent;
+    private static LocalDate mockedDate;
+
+    @BeforeAll
+    static void initAll(){
+        mockedEvent = new DateEvent();
+        mockedEvent.setEventName("Exam");
+        mockedEvent.setEventDescription("18/9/2015 - 12:00 to 15:00");
+
+        mockedDate = LocalDate.now().plusDays(7);
+        mockedEvent.setEventDate(mockedDate);
+    }
+
+    @AfterAll
+    static void tearDownAll(){
+        mockedEvent = null;
+        mockedDate = null;
+    }
+    @Test
+    void getEventName() {
+        assertEquals(mockedEvent.getEventName(), "Exam");
+    }
+
+    @Test
+    void getEventDate() {
+        assertEquals(mockedEvent.getEventDate(), mockedDate);
+    }
+
+    @Test
+    void getEventDescription() {
+        assertEquals(mockedEvent.getEventDescription(), "18/9/2015 - 12:00 to 15:00");
+    }
+}
